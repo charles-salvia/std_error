90d79ee51a80c2621200963bbbce89eb34a554b0
Charles Salvia <csalvia@hailoo.com>
2020-05-11 01:44:17 -0400

    Fix launder with clang

--- all_in_one.hpp
+++ all_in_one.hpp
@@ -1570,20 +1570,31 @@ class shared_string_ref : public string_ref
 
 #include <new>
 
+
 namespace stdx {
 
-#if __cplusplus < 201703L
+#if __cplusplus >= 201703L
+	#if defined(__cpp_lib_launder)
+		#define STDX_HAVE_NATIVE_LAUNDER 1
+		using std::launder;
+	#elif defined(STDX_CLANG_COMPILER)
+		#if __has_builtin(__builtin_launder)
+			#define STDX_HAVE_NATIVE_LAUNDER 1
+			template <class T>
+			constexpr T* launder(T* p) noexcept
+			{
+				return __builtin_launder(p);
+			}
+		#endif	
+	#endif
+#endif
 
+#if !defined(STDX_HAVE_NATIVE_LAUNDER)
 template <class T>
 constexpr T* launder(T* p) noexcept
 {
 	return p;
 }
-
-#else
-
-using std::launder;
-
 #endif
 
 } // end namespace stdx
